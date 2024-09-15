f8833a1b4ad6c7403b8ab457756b8c39951e052c
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2024-09-15 17:24:50 -0300

    feat(persistencia): adicionada persistência de dados

---
+++ src/RiverBooks.Books/Data/Migrations/20240915185948_Initial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RiverBooks.Books.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Books");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                schema: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,6)", precision: 18, scale: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Books",
+                table: "Books",
+                columns: new[] { "Id", "Author", "Price", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("0203517e-dc92-42c3-b6d5-267adec4faec"), "J.R.R. Tolkien", 19.99m, "O Retorno do Rei" },
+                    { new Guid("79329a9f-fac7-4f86-8224-e4e3792f0ce6"), "J.R.R. Tolkien", 14.99m, "As Duas Torres" },
+                    { new Guid("e47a0547-ff77-488f-9d2d-cf4ad659cf37"), "J.R.R. Tolkien", 10.99m, "A Sociedade do Anel" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books",
+                schema: "Books");
+        }
+    }
+}
