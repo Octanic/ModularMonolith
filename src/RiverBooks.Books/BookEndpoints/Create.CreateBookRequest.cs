299e45b70cf019efde037ada1aba6c47a09164f8
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2024-09-17 21:30:48 -0300

    feat(validator): adicionado validadores nas requests

--- src/RiverBooks.Books/BookEndpoints/Create.CreateBookRequest.cs
+++ src/RiverBooks.Books/BookEndpoints/Create.CreateBookRequest.cs
@@ -1,8 +1,35 @@
-﻿namespace RiverBooks.Books.BookEndpoints;
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace RiverBooks.Books.BookEndpoints;
 internal class CreateBookRequest
 {
 	public Guid? Id { get; set; }
 	public string Title { get; set; } = string.Empty;
 	public string Author { get; set; } = string.Empty;
 	public decimal Price { get; set; }
+}
+
+internal class CreateBookRequestValidator : Validator<CreateBookRequest>
+{
+	public CreateBookRequestValidator()
+	{
+		RuleFor(x => x.Id)
+					.NotNull()
+					.NotEqual(Guid.Empty)
+					.WithMessage("Um Id de livro é necessário");
+
+		RuleFor(x => x.Price)
+				.GreaterThanOrEqualTo(0)
+				.WithMessage("Preço não pode ser negativo");
+
+		RuleFor(x => x.Title)
+			.NotEmpty()
+			.WithMessage("Insira o nome do livro");
+
+		RuleFor(x => x.Author)
+			.NotEmpty()
+			.WithMessage("Insira o nome do autor");
+
+	}
 }
--- src/RiverBooks.Books/BookEndpoints/UpdatePrice.UpdateBookPriceRequest.cs
+++ src/RiverBooks.Books/BookEndpoints/UpdatePrice.UpdateBookPriceRequest.cs
@@ -1,2 +1,21 @@
-﻿namespace RiverBooks.Books.BookEndpoints;
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace RiverBooks.Books.BookEndpoints;
 public record UpdateBookPriceRequest(Guid Id, decimal NewPrice);
+
+public class UpdateBookPriceRequestValidator : Validator<UpdateBookPriceRequest>
+{
+	public UpdateBookPriceRequestValidator()
+	{
+		RuleFor(x => x.Id)
+				.NotNull()
+				.NotEqual(Guid.Empty)
+				.WithMessage("Um Id de livro é necessário");
+
+		RuleFor(x => x.NewPrice)
+				.GreaterThanOrEqualTo(0)
+				.WithMessage("Preço não pode ser negativo");
+
+	}
+}
