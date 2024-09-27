991afa8bc05330d6a4c4bb32ab920cd26315545c
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2024-09-27 19:35:19 -0300

    feat(Users): Adicionado módulo de usuários

---
+++ src/RiverBooks.Users/UsersModuleExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection;
+using Serilog;
+
+namespace RiverBooks.Users;
+
+public static class UsersModuleExtensions
+{
+	public static IServiceCollection AddUserModuleServices(
+		this IServiceCollection services,
+		ConfigurationManager config,
+		ILogger logger)
+	{
+		string? connectionString = config.GetConnectionString("UserConnectionString");
+		services.AddDbContext<UsersDbContext>(config =>
+				config.UseSqlServer(connectionString));
+
+		services.AddIdentityCore<ApplicationUser>()
+				.AddEntityFrameworkStores<UsersDbContext>();
+
+		logger.Information("Serviços do Módulo {Module} foram registrados.", "Users");
+
+		return services;
+	}
+}
+public class UsersDbContext : IdentityDbContext
+{
+	public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.HasDefaultSchema("Users");
+
+		builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+		base.OnModelCreating(builder);
+	}
+
+	protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+	{
+		configurationBuilder.Properties<decimal>()
+			.HavePrecision(18, 6);
+
+	}
+
+}
